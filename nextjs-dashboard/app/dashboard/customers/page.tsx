773373cd190c3f1a9feb400ea761b901b688b745
AxelHdz <axel.hernandez429@gmail.com>
2025-09-29 21:22:50 -0700

    Implemented complete Customers page

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,31 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import CustomersTableSkeleton from '@/app/ui/skeletons';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+        <CustomersTable customers={customers as FormattedCustomersTable[]} />
+      </Suspense>
+    </div>
+  );
+}
